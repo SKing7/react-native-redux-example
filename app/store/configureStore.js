c4fde364babaebbad5431ba380073b9159a05f7a
Jarret Moses <jarret.moses@gmail.com>
2017-02-25 16:02:48 -0500

    remove compose to reduce confusion

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,13 +1,12 @@
 // @flow
 
 // Redux Store Configuration
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState : Object) {
   const middleware = applyMiddleware(thunk);
-  const enhancer = compose(middleware);
 
-  return createStore(rootReducer, initialState, enhancer);
+  return createStore(rootReducer, initialState, middleware);
 }
