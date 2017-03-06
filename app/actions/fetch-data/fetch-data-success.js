32686c7352a543fad296fc9886b17e7f4dbdc726
Jarret Moses <jarret.moses@gmail.com>
2017-03-05 22:35:46 -0500

    update action

--- app/actions/fetch-data/fetch-data-success.js
+++ app/actions/fetch-data/fetch-data-success.js
@@ -5,6 +5,6 @@ import {FETCH_DATA_SUCCESS} from '../../constants/action-names';
 export const fetchDataSuccess = (weatherInfo : Object) => (
   {
     type: FETCH_DATA_SUCCESS,
-    payload: {weatherInfo},
+    payload: {...weatherInfo},
   }
 );
