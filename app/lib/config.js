469ae862497c826a42889a49ac3d1bded3c5d5f9
Jarret Moses <jarret.moses@gmail.com>
2017-12-12 10:30:51 -0500

    chore: update config file

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,9 +1,6 @@
-/*
- * Usually this config.js file would NEVER be commited to github repo
- * @flow
-*/
+// @flow
 
-const API_KEY = '2dbb276732a6602ce1de6bcb4bce61d8';
+const API_KEY = 'YOUR API KEY';
 
 export default {
   API_URL: `https://api.darksky.net/forecast/${API_KEY}/40.680925,-73.974663`,
